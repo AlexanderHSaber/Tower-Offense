5abb49a21e444dd624f561743c3a12bc353af061
george lee <geoliverlee@gmail.com>
2022-03-13 13:03:48 -0700

    adds projectile hit/damage vfx and tower rotating behavior

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,20 +16,23 @@ public class EnemyController : MonoBehaviour
     private float maxHealth;
     private HealthBarController healthBar;
 
+    private Material material;
+    private Coroutine flashRoutine;
+
     GameController gc;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
-
-        
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();        
 
         if (!tower) tower = GameObject.FindGameObjectWithTag("Player");
 
         maxHealth = health;
         healthBar = GetComponentInChildren<HealthBarController>();
+
+        material = GetComponent<SpriteRenderer>().material; // creates a new material instance for this renderer ; has to be destroyed manually before destroying gameobject
     }
 
     // Update is called once per frame
@@ -61,6 +64,9 @@ public class EnemyController : MonoBehaviour
         {
             health -= projectile.GetDamage();
 
+            //flash on hit
+            if (flashRoutine != null) StopCoroutine(flashRoutine);
+            flashRoutine = StartCoroutine(Flash(0.2f, 0.1f));
 
             //update health bar on damage
             if (healthBar)
@@ -70,14 +76,46 @@ public class EnemyController : MonoBehaviour
 
             if(health <= 0)
             {
-                Destroy(gameObject);
+                if (flashRoutine != null) StopCoroutine(flashRoutine);
+                Die();
             }
         }
 
         TowerController tower = collision.gameObject.GetComponent<TowerController>();
         if (tower)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    //lerp from alternate color to normal sprite appearance over rampTime
+    private IEnumerator Flash(float rampTime, float holdTime = 0)
+    {
+        //hold effect at full strength before lerp
+        if(holdTime > 0) {
+            material.SetFloat("_EffectStrength", 1);
+            yield return new WaitForSeconds(holdTime);
+        }
+
+        float startTime = Time.time;
+
+        //lerp back to normal appearance
+        while(Time.time - startTime < rampTime)
+        {           
+            float t = (Time.time - startTime) / rampTime;
+            float strength = Mathf.Lerp(1, 0, t);            
+            
+            material.SetFloat("_EffectStrength", strength);
+            
+            yield return null;
+        }
+
+        material.SetFloat("_EffectStrength", 0);
+    }
+
+    private void Die()
+    {
+        Destroy(material); //clean up material instance
+        Destroy(gameObject);
     }
 }
